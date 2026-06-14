44738deec24811dc0a9eebab2d9633a177e87dde
OxoGhost <arlenghost@proton.me>
2026-06-14 12:19:50 +0200

    feat: auto detection between start DE and start just shell

--- scripts/zsh-login.sh
+++ scripts/zsh-login.sh
@@ -2,5 +2,19 @@
 export HOME=/root
 export USER=root
 export LOGNAME=root
-exec -a -zsh /usr/bin/zsh
 
+if [ "$(tty)" = "/dev/tty1" ]; then
+    _active=$(cat "$HOME/.config/kira-desktop/active-de" 2>/dev/null)
+    _launcher="/usr/bin/kira-start-${_active}"
+    if [ -x "$_launcher" ]; then
+        # Wait for seatd
+        i=0
+        while [ ! -S /run/seatd.sock ] && [ $i -lt 30 ]; do
+            sleep 1
+            i=$((i + 1))
+        done
+        "$_launcher"
+    fi
+fi
+
+exec -a -zsh /usr/bin/zsh
